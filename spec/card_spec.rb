2a87ca66bf87563eb4457700393d3210d2a279db
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-05-12 10:07:36 -0600

    update

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,12 +1,339 @@
 require 'rspec'
 require './lib/card'
 
+# diamond
+
 RSpec.describe Card do
+  it "exists" do
+    card = Card.new(:diamond, 'King', 13)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+  
   it "exists" do
     card = Card.new(:diamond, 'Queen', 12)
 
     expect(card).to be_an_instance_of(Card)
   end
+
+  it "exists" do
+    card = Card.new(:diamond, 'Jack', 11)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:diamond, 'Ten', 10)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:diamond, 'Nine', 9)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:diamond, 'Eight', 8)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:diamond, 'Seven', 7)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:diamond, 'Six', 6)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:diamond, 'Five', 5)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:diamond, 'Four', 4)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:diamond, 'Three', 3)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:diamond, 'Two', 2)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:diamond, 'One', 1)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+
+
+  # spade
+
+  it "exists" do
+    card = Card.new(:spade, 'King', 13)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+  
+  it "exists" do
+    card = Card.new(:spade, 'Queen', 12)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:spade, 'Jack', 11)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:spade, 'Ten', 10)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:spade, 'Nine', 9)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:spade, 'Eight', 8)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:spade, 'Seven', 7)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:spade, 'Six', 6)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:spade, 'Five', 5)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:spade, 'Four', 4)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:spade, 'Three', 3)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:spade, 'Two', 2)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:spade, 'One', 1)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+
+  # club
+
+  it "exists" do
+    card = Card.new(:club, 'King', 13)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+  
+  it "exists" do
+    card = Card.new(:club, 'Queen', 12)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:club, 'Jack', 11)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:club, 'Ten', 10)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:club, 'Nine', 9)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:club, 'Eight', 8)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:club, 'Seven', 7)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:club, 'Six', 6)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:club, 'Five', 5)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:club, 'Four', 4)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:club, 'Three', 3)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:club, 'Two', 2)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:club, 'One', 1)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+
+
+  # heart
+
+  it "exists" do
+    card = Card.new(:heart, 'King', 13)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+  
+  it "exists" do
+    card = Card.new(:heart, 'Queen', 12)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:heart, 'Jack', 11)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:heart, 'Ten', 10)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:heart, 'Nine', 9)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:heart, 'Eight', 8)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:heart, 'Seven', 7)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:heart, 'Six', 6)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:heart, 'Five', 5)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:heart, 'Four', 4)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:heart, 'Three', 3)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:heart, 'Two', 2)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "exists" do
+    card = Card.new(:heart, 'One', 1)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+ ## Remember to make an ace for each suit^^
+ ## Figure out if its value is above a king
+ ## Or below a one
+
+ # replicate for all the rest of the cards N
+    # go through each suit one at a time Y
+    # double check that I've covered them all/checked them off the list N  
 
   it "has readable attributes" do
     card = Card.new(:diamond, 'Queen', 12)
@@ -16,3 +343,9 @@ RSpec.describe Card do
     expect(card.rank).to eq(12)
   end
 end
+
+# " " *use same strategy as above^^
+
+## Ask where the rules are for this game version on Slack
+## Someone mentioned somewhere it might be different than the traditional
+## Rules
