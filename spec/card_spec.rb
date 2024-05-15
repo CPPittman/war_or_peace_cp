fbb39c431422f3f071dfa0b7732b5e650c8ebd18
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-05-14 19:15:05 -0600

    fixing errors

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -77,11 +77,11 @@ RSpec.describe Card do
   end
 
   it "exists" do
-    card = Card.new(:diamond, 'Ace', 1)
+    card = Card.new(:diamond, 'Ace', 14)
 
     expect(card).to be_an_instance_of(Card)
   end
-
+end
 
 
   # spade
@@ -159,7 +159,7 @@ RSpec.describe Card do
   end
 
   it "exists" do
-    card = Card.new(:spade, 'Ace', 1)
+    card = Card.new(:spade, 'Ace', 14)
 
     expect(card).to be_an_instance_of(Card)
   end
@@ -240,7 +240,7 @@ RSpec.describe Card do
   end
 
   it "exists" do
-    card = Card.new(:club, 'Ace', 1)
+    card = Card.new(:club, 'Ace', 14)
 
     expect(card).to be_an_instance_of(Card)
   end
@@ -322,7 +322,7 @@ RSpec.describe Card do
   end
 
   it "exists" do
-    card = Card.new(:heart, 'Ace', 1)
+    card = Card.new(:heart, 'Ace', 14)
 
     expect(card).to be_an_instance_of(Card)
   end
@@ -432,16 +432,448 @@ RSpec.describe Card do
   end
 
   it "has readable attributes" do
-    card = Card.new(:diamond, 'Ace', 1)
+    card = Card.new(:diamond, 'Ace', 14)
 
     expect(card.suit).to eq(:diamond)
     expect(card.value).to eq('Ace')
     expect(card.rank).to eq(1)
   end
+
+  # spade
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'King', 13)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('King')
+    expect(card.rank).to eq(13)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Queen', 12)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Queen')
+    expect(card.rank).to eq(12)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Jack', 11)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Jack')
+    expect(card.rank).to eq(11)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Ten', 10)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Ten')
+    expect(card.rank).to eq(10)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Nine', 9)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Nine')
+    expect(card.rank).to eq(9)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Eight', 8)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Eight')
+    expect(card.rank).to eq(8)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Seven', 7)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Seven')
+    expect(card.rank).to eq(7)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Six', 6)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Six')
+    expect(card.rank).to eq(6)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Five', 5)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Five')
+    expect(card.rank).to eq(5)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Four', 4)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Four')
+    expect(card.rank).to eq(4)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Three', 3)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Three')
+    expect(card.rank).to eq(3)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Two', 2)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Two')
+    expect(card.rank).to eq(2)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Ace', 14)
+
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Ace')
+    expect(card.rank).to eq(1)
+  end
+
+  # club
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'King', 13)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('King')
+    expect(card.rank).to eq(13)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Queen', 12)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Queen')
+    expect(card.rank).to eq(12)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Jack', 11)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Jack')
+    expect(card.rank).to eq(11)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Ten', 10)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Ten')
+    expect(card.rank).to eq(10)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Nine', 9)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Nine')
+    expect(card.rank).to eq(9)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Eight', 8)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Eight')
+    expect(card.rank).to eq(8)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Seven', 7)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Seven')
+    expect(card.rank).to eq(7)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Six', 6)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Six')
+    expect(card.rank).to eq(6)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Five', 5)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Five')
+    expect(card.rank).to eq(5)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Four', 4)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Four')
+    expect(card.rank).to eq(4)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Three', 3)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Three')
+    expect(card.rank).to eq(3)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Two', 2)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Two')
+    expect(card.rank).to eq(2)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:club, 'Ace', 14)
+
+    expect(card.suit).to eq(:club)
+    expect(card.value).to eq('Ace')
+    expect(card.rank).to eq(1)
+  end
+
+  # heart
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'King', 13)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('King')
+    expect(card.rank).to eq(13)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Queen', 12)
+
+    expect(card.suit).to eq(heart)
+    expect(card.value).to eq('Queen')
+    expect(card.rank).to eq(12)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Jack', 11)
+
+    expect(card.suit).to eq(:heart
+    expect(card.value).to eq('Jack')
+    expect(card.rank).to eq(11)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Ten', 10)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Ten')
+    expect(card.rank).to eq(10)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Nine', 9)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Nine')
+    expect(card.rank).to eq(9)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Eight', 8)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Eight')
+    expect(card.rank).to eq(8)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Seven', 7)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Seven')
+    expect(card.rank).to eq(7)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Six', 6)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Six')
+    expect(card.rank).to eq(6)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Five', 5)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Five')
+    expect(card.rank).to eq(5)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Four', 4)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Four')
+    expect(card.rank).to eq(4)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Three', 3)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Three')
+    expect(card.rank).to eq(3)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Two', 2)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Two')
+    expect(card.rank).to eq(2)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:heart, 'Ace', 14)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Ace')
+    expect(card.rank).to eq(1)
+  end
+
+  # spade
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'King', 13)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('King')
+    expect(card.rank).to eq(13)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Queen', 12)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Queen')
+    expect(card.rank).to eq(12)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Jack', 11)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Jack')
+    expect(card.rank).to eq(11)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Ten', 10)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Ten')
+    expect(card.rank).to eq(10)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Nine', 9)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Nine')
+    expect(card.rank).to eq(9)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Eight', 8)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Eight')
+    expect(card.rank).to eq(8)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Seven', 7)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Seven')
+    expect(card.rank).to eq(7)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Six', 6)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Six')
+    expect(card.rank).to eq(6)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Five', 5)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Five')
+    expect(card.rank).to eq(5)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Four', 4)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Four')
+    expect(card.rank).to eq(4)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Three', 3)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Three')
+    expect(card.rank).to eq(3)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Two', 2)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Two')
+    expect(card.rank).to eq(2)
+  end
+
+  it "has readable attributes" do
+    card = Card.new(:spade, 'Ace', 14)
+
+    expect(card.suit).to eq(:spade)
+    expect(card.value).to eq('Ace')
+    expect(card.rank).to eq(1)
+  end
+
 end
 
 # " " *use same strategy as above^^
 
-## Ask where the rules are for this game version on Slack
-## Someone mentioned somewhere it might be different than the traditional
-## Rules
+# Create a Deck class and an accompanying test file with the following instance methods:
+
+# initialize: this method will take one argument - an array of cards.
+# cards: an attr_reader to read the @cards attribute
+# rank_of_card_at: this method will take one argument that represents the index location of a card to be used (typically 0 or 2 more on this later 😉 ) and will return the rank of that card.
+# high_ranking_cards: this method will return an array of cards in the deck that have a rank of 11 or above (face cards and aces)
+# percent_high_ranking: this method will return the percentage of cards that are high ranking
+# remove_card: this method will remove the top card from the deck
+# add_card: this method will add one card to the bottom (end) of the deck
+# Use the interaction pattern below to help you build your Deck test and Deck class.
